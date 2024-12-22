4d8e8522cab0221a113b738866581bb490b677bb
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2024-12-22 11:52:35 +0200

    Added roles decorator to word methods update, create and delete
    users with role 'User'now can only fetch words while users with role 'Admin' can do CRUD operations with words

--- src/word/word.controller.ts
+++ src/word/word.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { WordService } from './word.service';
 import { CreateWordDto } from './dto/create-word.dto';
 import { UpdateWordDto } from './dto/update-word.dto';
 import { Word } from './entities/word.entity'
+import { RolesGuard } from '../roles/roles.guard';
+import { Roles } from 'src/roles/roles.decorator';
+import { Role } from 'src/roles/enums/role.enum';
+
 
 @Controller('word')
+@UseGuards(RolesGuard)
 export class WordController {
   constructor(private readonly wordService: WordService) {}
 
   @Post()
+  @Roles(Role.Admin)
   async create(@Body() createWordDto: CreateWordDto): Promise<Word> {
     return this.wordService.create(createWordDto);
   }
@@ -24,6 +30,7 @@ export class WordController {
   }
 
   @Patch(':id')
+  @Roles(Role.Admin)
   async update(
     @Param('id') id: string,
     @Body() updateWordDto: UpdateWordDto,
@@ -32,6 +39,7 @@ export class WordController {
   }
 
   @Delete(':id')
+  @Roles(Role.Admin)
   async remove(@Param('id') id: string): Promise<void> {
     return this.wordService.delete(+id);
   }
