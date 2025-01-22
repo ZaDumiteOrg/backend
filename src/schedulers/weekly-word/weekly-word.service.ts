c830a3c54029691ab63e5e37ce38b2fc169d5530
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-22 12:37:31 +0200

    Scheduler to assign weekly words
    Set to assign new word every hour for testing purposes

---
+++ src/schedulers/weekly-word/weekly-word.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { UserService } from '../../user/user.service';
+
+@Injectable()
+export class WeeklyWordService {
+  constructor(private readonly userService: UserService) {}
+
+  @Cron('0 * * * *')
+  async assignWeeklyWord() {
+    console.log('Scheduler triggered: Assigning word of the week...');
+
+    try {
+      const assignedWord = await this.userService.assignWordOfTheWeekToUsers();
+      console.log('Assigned Word of the Week:', assignedWord);
+      console.log('Word of the week assigned successfully.');
+    } catch (error) {
+      console.error('Error assigning word of the week:', error.message);
+    }
+  }
+}
