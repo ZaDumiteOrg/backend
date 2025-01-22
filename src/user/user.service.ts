3d3a028e426f96b25375764a132b149ae5cc64c2
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-22 12:30:38 +0200

    Methods to assign word of week and fetch the users words

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,12 +6,16 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Role } from '../roles/enums/role.enum'
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Word } from '../word/entities/word.entity';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+
+    @InjectRepository(Word) 
+    private readonly wordRepository: Repository<Word>,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -71,5 +75,52 @@ export class UserService {
     if (result.affected === 0) {
       throw new NotFoundException(`User with ID ${id} not found`);
     }
+  }
+
+  async assignWordOfTheWeekToUsers(): Promise<Word> {
+    const now = new Date();
+    const startOfYear = new Date(now.getFullYear(), 0, 1);
+    const currentWeek = Math.floor((now.getTime() - startOfYear.getTime()) / (7 * 24 * 60 * 60 * 1000)) + 1;
+  
+    const totalWords = await this.wordRepository.count();
+  
+    if (totalWords === 0) {
+      throw new Error('No words available in the database.');
+    }
+  
+    const wordWeek = ((currentWeek - 1) % totalWords) + 1;
+  
+    const wordOfTheWeek = await this.wordRepository.findOneBy({ week: wordWeek });
+    console.log("Word from user service:", wordOfTheWeek)
+  
+    if (!wordOfTheWeek) {
+      throw new Error(`No word found for week ${wordWeek}`);
+    }
+  
+    const users = await this.userRepository.find({
+      relations: ['words'], 
+    });
+  
+    for (const user of users) {
+      if (!user.words.some((word) => word.id === wordOfTheWeek.id)) {
+        user.words.push(wordOfTheWeek);
+        await this.userRepository.save(user);
+      }
+    }
+    return wordOfTheWeek;
+  }
+  
+
+  async getUserWords(userId: number): Promise<Word[]> {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['words'], 
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+
+    return user.words;
   }
 }
