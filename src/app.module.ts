72e496b8b47ceeed0d6f6432106580409d7031f6
katpile <ekaterina.m.dimitrova.2020@elsys-bg.org>
2025-01-29 13:08:42 +0200

    Updated app module to use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,36 +7,43 @@ import { User } from './user/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { jwtConstants } from './auth/constants';
 import { JwtModule } from '@nestjs/jwt';
-import { RolesGuard } from './roles/roles.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { WordModule } from './word/word.module';
 import { Word } from './word/entities/word.entity';
 import { AuthGuard } from './auth/auth.guard';
-import { AuthController } from './auth/auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'zadumite',
-      entities: [User, Word],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [User, Word],
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE') ?? false,
+      }),
     }),
     JwtModule.register({
-      global: true, 
+      global: true,
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '10h' },
     }),
     UserModule,
     WordModule,
-    AuthModule
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService,  
+  providers: [
+    AppService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
